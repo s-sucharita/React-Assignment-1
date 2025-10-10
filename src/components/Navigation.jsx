938ce6b842cf88d785259719382503e73349d99e
s-sucharita <sucharita5991@gmail.com>
2025-10-10 23:21:09 +0530

    Altered

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -12,24 +12,29 @@ function Navbar({ onSearch, onSort, onFilter }) {
         <ul className="nav-links flex space-x-10">
           <li>
           <select
-            className="p-2 rounded"
+            className="p-2 rounded text-amber-50"
             onChange={(e) => onSort(e.target.value)}
           >
-            <option value="">Sort By</option>
+            
+             <option value="">Sort By</option>
+             <div class="text-black">
             <option value="priceLowHigh">Price: Low to High</option>
             <option value="priceHighLow">Price: High to Low</option>
             <option value="ratingHighLow">Rating: High to Low</option>
             <option value="ratingLowHigh">Rating: Low to High</option>
+             </div>
           </select>
         </li>
 
        
         <li>
           <select
-            className="p-2 rounded"
+            className="p-2 rounded text-amber-50"
             onChange={(e) => onFilter(e.target.value)}
           >
+            
             <option value="">All Categories</option>
+            <div class="text-black">
             <option value="Earrings">Earrings</option>
             <option value="Nose Ring">Nose Ring</option>
             <option value="Anklets">Anklets</option>
@@ -37,6 +42,7 @@ function Navbar({ onSearch, onSort, onFilter }) {
             <option value="Bangles">Bangles</option>
             <option value="Maati">Maati</option>
             <option value="Nettichutti">Nettichutti</option>
+            </div>
           </select>
         </li>
         
--- src/components/Products.js
+++ src/components/Products.js
@@ -1,229 +1,30 @@
-const Products = [
-  {
-    id: 1,
-    name: "Nakshi Bridal Combo set",
-    price: 2399.00,
-    category: "chain",
-    rating: 4,
-    image: "public/pictures/nakshi-bridalcombo-2000.webp"
-  },
-  {
-    id: 2,
-    name: "Diamond Studs Earrings",
-    price: 899.00,
-    category: "Earrings",
-    rating: 5,
-    image: "public/pictures/diamond-studs-earrings.webp"
-  },
-  {
-    id: 3,
-  name: "Durgavati Bridal Combo Set",
-    price: 2599.00,
-    category: "Chain",
-    rating: 4,
-    image: "public/pictures/durgavati-bridalset-2500.webp"
-  },
-  {
-       id: 4,
-    name: "Diamond Nose ring",
-    price: 199.99,
-    category: "Nose Ring",
-    rating: 5,
-    image: "public/pictures/diamond-nosering.webp"
-  },
-  {
-    id: 5,
-    name: "Ghungroo Anklets",
-    price: 399.99,
-    category: "Anklets",
-    rating: 5,
-    image: "public/pictures/Ghungroo-Anklets.webp"
-  },
-  {
-    id: 6,
-    name: "Invisible Lotus Chain",
-    price: 149.99,
-    category: "Chain",
-    rating: 4,
-    image: "public/pictures/invisiblechain.jpeg"
-    },
-    {
-     id: 7,
-    name: "Invisible Chain with Earrings",
-    price: 299.99,
-    category: "Chain",
-    rating: 4,
-    image: "public/pictures/invisiblechainearrings.jpg"
-  },
-  {
-    id: 8,
-    name: "Invisible Chain Pendant",
-    price: 199.00,
-    category: "Chain",
-    rating: 5,
-    image: "public/pictures/invisiblechaingpendent.webp"
-  },
-  {
-    id: 9,
-    name: "Kashu Mala Set",
-    price: 999.00,
-    category: "Chain",
-    rating: 4.7,
-    image: "public/pictures/kashu-malaset-2400.webp"
-  },
-    {
-    id: 10,
-    name: "Kemp Jhumkas",
-    price: 399.00,
-    category: "Earrings",
-    rating: 5,
-    image: "public/pictures/kemp-jhumkas.webp"
-  },
-    {
-    id: 11,
-    name: "Kemp Anklet",
-    price: 199.00,
-    category: "Anklets",
-    rating: 5,
-    image: "public/pictures/kemp-anklet-1300.webp"
-  },
-    {
-    id: 12,
-    name: "Kemp Necklace",
-    price: 799.00,
-    category: "Chain",
-    rating: 4,
-    image: "public/pictures/kemp-necklace.webp"
-  },
-    {
-    id: 13,
-    name: "Kemp Nettichutti",
-    price: 199.00,
-    category: "Nettichutti",
-    rating: 5,
-    image: "public/pictures/kemp-nettichutti.webp"
-  },
-    {
-    id: 14,
-    name: "Palakka Necklace",
-    price: 1299.00,
-    category: "Chain",
-    rating: 5,
-    image: "public/pictures/Kerala-Palakka-Necklace.jpg"
-  },
-    {
-    id: 15,
-    name: "Lakshmi Jhumkas",
-    price: 799.00,
-    category: "Earrings",
-    rating: 4,
-    image: "public/pictures/lakshmi-jhumkas.webp"
-  },
-    {
-    id: 16,
-    name: "Lakshmi Nettichutti",
-    price: 399.00,
-    category: "Nettichutti",
-    rating: 5,
-    image: "public/pictures/lakshmi-nettichutti.webp"
-  },
-    {
-    id: 17,
-    name: "Lotus Anklet",
-    price: 199.00,
-    category: "Anklets",
-    rating: 5,
-    image: "public/pictures/lotus-anklet.webp"
-  },
-    {
-    id: 18,
-    name: "Maati Jhumkas",
-    price: 399.00,
-    category: "Maati",
-    rating: 4,
-    image: "public/pictures/maati-jhumkas-1300.webp"
-  },
-    {
-    id: 19,
-    name: "Nagas Haram",
-    price: 1799.00,
-    category: "Chain",
-    rating: 5,
-    image: "public/pictures/nagas-haram.webp"
-  },
-    {
-    id: 20,
-    name: "Nagas Nettichutti",
-    price: 299.00,
-    category: "Nettichutti",
-    rating: 5,
-    image: "public/pictures/nettichutti.webp"
-  },
-  {
-    id: 21,
-    name: "Palakka Necklace with Earrings",
-    price: 1299.00,
-    category: "Chain",
-    rating: 5,
-    image: "public/pictures/palakka-necklace-earrings.webp"
-  },
-  {
-    id: 22,
-    name: "Padmini Bangles",
-    price: 899.00,
-    category: "Bangles",
-    rating: 5,
-    image: "public/pictures/padmini-bangle-1700.webp"
-  },
-  {
-    id: 23,
-    name: "Peacock Maati",
-    price: 299.00,
-    category: "Maati",
-    rating: 4,
-    image: "public/pictures/peacock-maati.webp"
-  },
-  {
-    id: 24,
-    name: "Pearl Maati",
-    price: 299.00,
-    category: "Maati",
-    rating: 4,
-    image: "public/pictures/peral-maati-1500.webp"
-  },
-  {
-    id: 25,
-    name: "Roja Bangles",
-    price: 799.00,
-    category: "Bangles",
-    rating: 3,
-    image: "public/pictures/roja-bangles.webp"
-  },
-  {
-    id: 26,
-    name: "surya Nose ring",
-    price: 799.00,
-    category: "Nose Ring",
-    rating: 3,
-    image: "public/pictures/surya-nosering.webp"
-  },
-  {
-    id: 27,
-    name: "Triangle Necklace",
-    price: 799.00,
-    category: "Chain",
-    rating: 3,
-    image: "public/pictures/triangle-necklace.webp"
-  },
-  {
-    id: 28,
-    name: "Palakka Bangles",
-    price: 699.00,
-    category: "Bangles",
-    rating: 3,
-    image: "public/pictures/palakka-bangle.webp"
-  }
-
-];
-
-export default Products;
+const products = [
+     { "id": 1, "name": "Nakshi Bridal Combo set", "price": 2399.00, "category": "chain", "rating": 4, "image": "public/pictures/nakshi-bridalcombo-2000.webp" },
+     { "id": 2, "name": "Diamond Studs Earrings", "price": 899.00, "category": "Earrings", "rating": 5, "image": "public/pictures/diamond-studs-earrings.webp" }, 
+     { "id": 3, "name": "Durgavati Bridal Combo Set", "price": 2599.00, "category": "Chain", "rating": 4, "image": "public/pictures/durgavati-bridalset-2500.webp" },
+     { "id": 4, "name": "Diamond Nose ring", "price": 199.99, "category": "Nose Ring", "rating": 5, "image": "public/pictures/diamond-nosering.webp" }, 
+     { "id": 5, "name": "Ghungroo Anklets", "price": 399.99, "category": "Anklets", "rating": 5, "image": "public/pictures/Ghungroo-Anklets.webp" }, 
+     { "id": 6, "name": "Invisible Lotus Chain", "price": 149.99, "category": "Chain", "rating": 4, "image": "public/pictures/invisiblechain.jpeg" }, 
+     { "id": 7, "name": "Invisible Chain with Earrings", "price": 299.99, "category": "Chain", "rating": 4, "image": "public/pictures/invisiblechainearrings.jpg" }, 
+     { "id": 8, "name": "Invisible Chain Pendant", "price": 199.00, "category": "Chain", "rating": 5, "image": "public/pictures/invisiblechaingpendent.webp" }, 
+     { "id": 9, "name": "Kashu Mala Set", "price": 999.00, "category": "Chain", "rating": 4.7, "image": "public/pictures/kashu-malaset-2400.webp" }, 
+     { "id": 10, "name": "Kemp Jhumkas", "price": 399.00, "category": "Earrings", "rating": 5, "image": "public/pictures/kemp-jhumkas.webp" }, 
+     { "id": 11, "name": "Kemp Anklet", "price": 199.00, "category": "Anklets", "rating": 5, "image": "public/pictures/kemp-anklet-1300.webp" }, 
+     { "id": 12, "name": "Kemp Necklace", "price": 799.00, "category": "Chain", "rating": 4, "image": "public/pictures/kemp-necklace.webp" }, 
+     { "id": 13, "name": "Kemp Nettichutti", "price": 199.00, "category": "Nettichutti", "rating": 5, "image": "public/pictures/kemp-nettichutti.webp" }, 
+     { "id": 14, "name": "Palakka Necklace", "price": 1299.00, "category": "Chain", "rating": 5, "image": "public/pictures/Kerala-Palakka-Necklace.jpg" }, 
+     { "id": 15, "name": "Lakshmi Jhumkas", "price": 799.00, "category": "Earrings", "rating": 4, "image": "public/pictures/lakshmi-jhumkas.webp" }, 
+     { "id": 16, "name": "Lakshmi Nettichutti", "price": 399.00, "category": "Nettichutti", "rating": 5, "image": "public/pictures/lakshmi-nettichutti.webp" }, 
+     { "id": 17, "name": "Lotus Anklet", "price": 199.00, "category": "Anklets", "rating": 5, "image": "public/pictures/lotus-anklet.webp" }, 
+     { "id": 18, "name": "Maati Jhumkas", "price": 399.00, "category": "Maati", "rating": 4, "image": "public/pictures/maati-jhumkas-1300.webp" }, 
+     { "id": 19, "name": "Nagas Haram", "price": 1799.00, "category": "Chain", "rating": 5, "image": "public/pictures/nagas-haram.webp" }, 
+     { "id": 20, "name": "Nagas Nettichutti", "price": 299.00, "category": "Nettichutti", "rating": 5, "image": "public/pictures/nettichutti.webp" }, 
+     { "id": 21, "name": "Palakka Necklace with Earrings", "price": 1299.00, "category": "Chain", "rating": 5, "image": "public/pictures/palakka-necklace-earrings.webp" }, 
+     { "id": 22, "name": "Padmini Bangles", "price": 899.00, "category": "Bangles", "rating": 5, "image": "public/pictures/padmini-bangle-1700.webp" }, 
+     { "id": 23, "name": "Peacock Maati", "price": 299.00, "category": "Maati", "rating": 4, "image": "public/pictures/peacock-maati.webp" }, 
+     { "id": 24, "name": "Pearl Maati", "price": 299.00, "category": "Maati", "rating": 4, "image": "public/pictures/peral-maati-1500.webp" }, 
+     { "id": 25, "name": "Roja Bangles", "price": 799.00, "category": "Bangles", "rating": 3, "image": "public/pictures/roja-bangles.webp" }, 
+     { "id": 26, "name": "surya Nose ring", "price": 799.00, "category": "Nose Ring", "rating": 3, "image": "public/pictures/surya-nosering.webp" }, 
+     { "id": 27, "name": "Triangle Necklace", "price": 799.00, "category": "Chain", "rating": 3, "image": "public/pictures/triangle-necklace.webp" }, 
+     { "id": 28, "name": "Palakka Bangles", "price": 699.00, "category": "Bangles", "rating": 3, "image": "public/pictures/palakka-bangle.webp" } ]; 
+export default products;
